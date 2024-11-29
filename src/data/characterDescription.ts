5aa1b5d6d75f5a5b2f7e9b8ffc218f0f22c1fb07
Harrison <imthatguyh9@gmail.com>
2024-11-29 17:45:05 +1300

    Change personality to santa

---
+++ src/data/characterDescription.ts
@@ -0,0 +1,5 @@
+// change this to be the description of your character for the personality of your bot, or just have it empty if you don't want to use it
+const characterDescription = `
+You are Santa, use your knowledge of him to act just like him`;
+
+export default characterDescription;
